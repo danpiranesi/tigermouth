c5e9563e54b88206bd6667db734ec91a6ce654dd
danpiranesi <110695726+danpiranesi@users.noreply.github.com>
2024-02-08 22:25:09 -0700

    UI Switchup

--- tiger/src/app/_components/Logo.tsx
+++ tiger/src/app/_components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Logo = () => {
     return (
       <div className="flex items-center">
-        <Image src="images/TigerMouthLogo.svg" alt="Logo" height={80} width={80} />
+        <Image src="images/TigerMouthLogo.svg" alt="Logo" height={60} width={60} />
       </div>
     );
   };
